1d4bf086eaafbfcfa65388aaa71e56b0e3510e58
Klaus Townsend <subjunk@gmail.com>
2022-08-14 09:03:37 +1200

    Added Nx with two Dockerized apps and helm chart

---
+++ apps/api/src/app/app.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { Todo } from '@azure-poc/todos';
+
+@Injectable()
+export class AppService {
+  getData(): Todo[] {
+    return [
+      { message: 'Is Kubernetes still awesome?', done: true },
+      { message: 'Will we ever want to go back to Heroku?', done: false },
+    ];
+  }
+}
---
+++ libs/todos/README.md
@@ -0,0 +1,11 @@
+# todos
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test todos` to execute the unit tests via [Jest](https://jestjs.io).
+
+## Running lint
+
+Run `nx lint todos` to execute the lint via [ESLint](https://eslint.org/).
